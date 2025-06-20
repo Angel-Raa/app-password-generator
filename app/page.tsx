9e54e1647272643280bdf19eb61d09aa7bafcc2a
Angel Aguero <angelagueror23@gmail.com>
2025-06-19 22:08:30 -0400

    feat: initialize Prisma setup with SQLite database and Password model
    - Added Prisma client configuration in `lib/database/prisma.ts`.
    - Implemented `QueryProvider` for React Query in `lib/provider/query-provider.tsx`.
    - Created SQLite database file `prisma/dev.db` with initial schema.
    - Defined `Password` model in `prisma/schema.prisma` with relevant fields.
    - Generated migration files for the initial database setup.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FormCreatePassword } from "@/components/password/form-create-password"
-
-export default function Home() {
-  return (
-    <main>
-      <FormCreatePassword />
-    </main>
-  )
-}
